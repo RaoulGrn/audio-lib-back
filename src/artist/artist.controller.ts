bc34ba89f6d30f0dfb90be0b86ca84c3fd8ac794
Raoul <grnraoul@gmail.com>
2024-07-04 20:28:51 +0300

    artist controller class

--- src/artist/artist.controller.ts
+++ src/artist/artist.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Query,
+} from '@nestjs/common';
+import { ArtistService } from './artist.service';
+import { CreateArtistDto } from './dto/create-artist.dto';
 
 @Controller('artist')
-export class ArtistController {}
+export class ArtistController {
+  constructor(private readonly artistService: ArtistService) {}
+
+  @Get()
+  findAll() {
+    return this.artistService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.artistService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() createArtistDto: CreateArtistDto) {
+    return this.artistService.create(createArtistDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.artistService.remove(id);
+  }
+
+  @Get('search')
+  search(@Query('name') name: string) {
+    return this.artistService.search(name);
+  }
+}
